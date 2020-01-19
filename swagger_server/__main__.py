258f24adf794865589342dd8f16f64ff888a5850
Martin Robinson <martinjrobins@gmail.com>
2020-01-19 08:17:33 +0000

    update server path

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -8,6 +8,5 @@ app = connexion.App(__name__, specification_dir='./swagger/')
 app.app.json_encoder = encoder.JSONEncoder
 app.add_api('swagger.yaml', arguments={'title': 'PINTS problem benchmark'}, pythonic_params=True)
 
-
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run()
