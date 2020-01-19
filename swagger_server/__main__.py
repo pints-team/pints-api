633e0722fd1c3d859f8506cc15a790bcaa31c314
Martin Robinson <martinjrobins@gmail.com>
2020-01-19 08:05:02 +0000

    add unicorn for deploy to heroku

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -4,13 +4,10 @@ import connexion
 
 from swagger_server import encoder
 
-
-def main():
-    app = connexion.App(__name__, specification_dir='./swagger/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'PINTS problem benchmark'}, pythonic_params=True)
-    app.run(port=8080)
+app = connexion.App(__name__, specification_dir='./swagger/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('swagger.yaml', arguments={'title': 'PINTS problem benchmark'}, pythonic_params=True)
 
 
 if __name__ == '__main__':
-    main()
+    app.run(port=8080)
